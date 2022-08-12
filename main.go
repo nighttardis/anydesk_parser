abe656cb6c621de8f8a82246f3063f9bc923e179
nighttardis <nighttardis1701@gmail.com>
2022-08-12 15:12:12 -0500

    updates to parse and profile sessions

--- main.go
+++ main.go
@@ -6,13 +6,15 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	logreg := `^\s+(?P<loglevel>[^\s]+)\s(?P<datetime>[\d-]+\s[\d:\.]+)\s+(?P<appname>\w+)\s+(?P<threadname>[a-z][^\s]+)?\s+(?P<pid>\d+)\s+(?P<threadid>\d+)\s+(?P<fiberid>\d+)?\s+(?P<functionname>[^\s]+)\s+-(?P<message>.+)`
 	logre := regexp.MustCompile(logreg)
 
-	file, err := os.Open("data/ad.trace_windows_user_approval")
+	file, err := os.Open("data/test_data")
 
 	if err != nil {
 		fmt.Println(err)
@@ -33,7 +35,13 @@ func main() {
 		le := make(map[string]interface{})
 		for i, name := range logre.SubexpNames() {
 			if i != 0 {
-				le[name] = match[i]
+				switch name {
+				case "pid", "fiberid", "threadid":
+					le[name], _ = strconv.Atoi(match[i])
+				default:
+					le[name] = strings.TrimSpace(match[i])
+				}
+
 			}
 		}
 		jsonbody, _ := json.Marshal(le)
@@ -42,12 +50,17 @@ func main() {
 
 		leS.parseFunction(&ads)
 
-		if ads.SessionStart {
+		if ads.SessionStart && leS.FunctionName != "main" {
 			ads.LogEntries = append(ads.LogEntries, leS)
 		}
 
 		if ads.SessionEnd {
-			fmt.Println(ads)
+			body, _ := json.Marshal(ads)
+			fmt.Println(string(body))
+			if !ads.SessionStart {
+				fmt.Println("[+] WARNING Found session need before finding a session begin.")
+				fmt.Println("[+] WARNING This could be because of rolled logs or parsing errors.")
+			}
 			ads = AnydeskSession{}
 		}
 
